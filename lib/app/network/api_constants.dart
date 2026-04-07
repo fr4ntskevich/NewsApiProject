7817a238e2b3b91349c1d8f9e212b2b886a685cb
fr4ntskevichh <fr4ntskevichh@gmail.com>
2026-04-07 10:13:04 +0300

    Remove helper api keys

--- lib/app/network/api_constants.dart
+++ lib/app/network/api_constants.dart
@@ -1,8 +1,6 @@
 class ApiConstants {
   static const String baseUrl = 'https://newsapi.org/v2/';
   static const String apiKey = '639245bed15d4aee8bd36637642e134f';
-  // static const String apiKey = 'd1b777ffc2e44770ad2284a1d1696618';
-  // static const String apiKey = '5451476c399047c2a77a6a332bff4f2b';
 
   static const Duration connectTimeout = Duration(seconds: 30);
   static const Duration receiveTimeout = Duration(seconds: 30);
